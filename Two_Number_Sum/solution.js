aa72d5dfafe31fa6dae2d6ca24d6802138cdf506
Lixuan Luo <lixuanluo@hotmail.com>
2021-05-18 10:58:50 -0400

    add solution2 and solution3

--- Two_Number_Sum/solution.js
+++ Two_Number_Sum/solution.js
@@ -12,7 +12,40 @@ function twoNumberSum(array, targetSum) {
   }
   return [];
 }
+exports.twoNumberSum = twoNumberSum;
 
 // Solution 2
+// O(n) time | O(n) space
+function twoNumberSum(array, targetSum) {
+  const nums = {};
+  for (const num of array) {
+    const potentialMatch = targetSum - num;
+    if (potentialMatch in nums) {
+      return [potentialMatch, num];
+    } else {
+      nums[num] = true;
+    }
+  }
+  return [];
+}
+exports.twoNumberSum = twoNumberSum;
 
 // Solution 3
+// O(nlog(n)) time | O(1) space
+function twoNumberSum(array, targetSum) {
+  array.sort((a, b) => a - b);
+  let left = 0;
+  let right = array.length - 1;
+  while (left < right) {
+    const currentSum = array[left] + array[right];
+    if (currentSum === targetSum) {
+      return [array[left], array[right]];
+    } else if (currentSum < targetSum) {
+      left++;
+    } else if (current > targetSum) {
+      right--;
+    }
+  }
+  return [];
+}
+exports.twoNumberSum = twoNumberSum;
