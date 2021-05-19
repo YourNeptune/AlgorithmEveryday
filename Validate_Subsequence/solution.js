88a57b541b267489c5b15a5ba9b3e242afc3b899
Lixuan Luo <lixuanluo@hotmail.com>
2021-05-19 15:26:26 -0400

    update

--- Validate_Subsequence/solution.js
+++ Validate_Subsequence/solution.js
@@ -1,25 +1,24 @@
 // Solution 1
 // O(n) time | O(1) space - where n is the length of the array
 function isValidSubsequence(array, sequence) {
-    let arrIdx = 0
-    let seqIdx = 0;
-    while(arrIdx < array.length && seqIdx < sequence.length){
-        if(array[arrIdx] === sequence[seqIdx]) seqIdx++
-        arrIdx++
-    }
-    return seqIdx === sequence.length
+  let arrIdx = 0;
+  let seqIdx = 0;
+  while (arrIdx < array.length && seqIdx < sequence.length) {
+    if (array[arrIdx] === sequence[seqIdx]) seqIdx++;
+    arrIdx++;
+  }
+  return seqIdx === sequence.length;
 }
 
 exports.isValidSubsequence = isValidSubsequence;
 
-
 // Solution 2
 // O(n) time | O(1) space
 function isValidSubsequence(array, sequence) {
-    let seqIdx = 0
-    for(const value of array) {
-        if(seqIdx === sequence.length) break
-        if(sequence[seqIdx] === value) seqIdx++
-    }
-    return seqIdx === sequence.length
+  let seqIdx = 0;
+  for (const value of array) {
+    if (seqIdx === sequence.length) break;
+    if (sequence[seqIdx] === value) seqIdx++;
+  }
+  return seqIdx === sequence.length;
 }
